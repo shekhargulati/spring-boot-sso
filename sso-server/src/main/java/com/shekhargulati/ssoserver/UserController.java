ad2197a04052725942b76742437d2c9ede91ca00
Shekhar Gulati <shekhargulati84@gmail.com>
2018-12-25 13:15:12 +0530

    Spring Boot SSO blog repository

---
+++ sso-server/src/main/java/com/shekhargulati/ssoserver/UserController.java
@@ -0,0 +1,15 @@
+package com.shekhargulati.ssoserver;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/me")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
